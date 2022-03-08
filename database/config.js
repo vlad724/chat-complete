9553f51f4f277562e534ea2712c9d8aab987dc64
vlad724 <lokil724@gmail.com>
2022-03-08 17:08:59 -0300

    otro commit

--- database/config.js
+++ database/config.js
@@ -8,6 +8,7 @@ const dbConnection = async() => {
         await mongoose.connect(process.env.MONGODB_CNN, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            useCreateIndex: true
 
         });
 
